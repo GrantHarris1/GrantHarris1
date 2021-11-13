b4ce69c5212d581652076f20914dc44777736690
grantharriselectric <grantharriselectric@gmail.com>
2021-11-12 19:18:05 -0500

    firebase

--- src/components/Headliner.js
+++ src/components/Headliner.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 
-
 export default function Headliner() {
+
     return (
         <>
             <title> Grant's Portfolio</title>
@@ -15,12 +15,19 @@ export default function Headliner() {
                         <Nav className="me-auto">
                             <Nav.Link as={Link} to='/aboutme'>AboutMe</Nav.Link>
                             <Nav.Link as={Link} to='/projects'>Projects</Nav.Link>
-                            <Nav.Link  href='https://github.com/GrantHarris1'><i class="bi bi-github text-secondary" id="git"></i></Nav.Link >
-                            <Nav.Link  href='https://www.linkedin.com/in/1grantharris1/'><i class="bi bi-linkedin text-secondary" id="git"></i></Nav.Link >
+                            <Nav.Link href='https://github.com/GrantHarris1'><i class="bi bi-github text-secondary" id="git"></i></Nav.Link >
+                            <Nav.Link href='https://www.linkedin.com/in/1grantharris1/'><i class="bi bi-linkedin text-secondary" id="git"></i></Nav.Link >
+                            <Nav.Link
+                                data-tf-popup="HWfJz59o"
+                            //style="all:unset;font-family:Helvetica,Arial,sans-serif;display:inline-block;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;background-color:#0445AF;color:#FFFFFF;font-size:20px;border-radius:25px;padding:0 33px;font-weight:bold;height:50px;cursor:pointer;line-height:50px;text-align:center;margin:0;text-decoration:none;"
+                            >
+                                Lets Connect
+                            </Nav.Link>
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
-            </Navbar>
+               </Navbar>
 
 
         </>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,174 +2,171 @@ import React from 'react'
 import { Carousel, Container } from 'react-bootstrap'
 //  import {img} from "./img/weathermoblie.png"
 export default function Projects() {
-    
-        
-    
+
+
+
     return (
-        <body>
-        <Container>
-            <br />
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="/img/fulltodo.png"
-                        alt="Second slide"
-                    />
 
-                    <Carousel.Caption>
-                        <div className="border-dark  d-none d-md-block" id='todocap' >
-                            
-                            <h2 className='text-light'>React Project :</h2>
-                            <p id="pic">
-                                A React Project Todo list App using React, CSS and Bootstrap<br />
-                            </p>
-                            <h1>
-                                <a href="https://github.com/GrantHarris1/todo"><i className="bi bi-github"></i></a>
-                            </h1>
+            <Container className='projectCon'>
+                <br />
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="/img/fulltodo.png"
+                            alt="Second slide"
+                        />
+
+                        <Carousel.Caption>
+                            <div className="border-dark  d-none d-md-block" id='todocap' >
+
+                                <h2 className='text-light'>React Project :</h2>
+                                <p id="pic">
+                                    A React Project Todo list App using React, CSS and Bootstrap<br />
+                                </p>
+                                <h1>
+                                    <a href="https://github.com/GrantHarris1/todo"><i className="bi bi-github"></i></a>
+                                </h1>
+                            </div>
+
+                        </Carousel.Caption>
+                        <div className='smallCap text-center' id="smallCap">
+                            <a href="https://github.com/GrantHarris1/todo" id='a'>React.  <i className='bi bi-github'></i></a>
                         </div>
 
-                    </Carousel.Caption>
-                    <div className='smallCap text-center' id="smallCap">
-                    <a href="https://github.com/GrantHarris1/todo" id ='a'>React.  <i className='bi bi-github'></i></a>
-                    </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="/img/newHero.png"
+                            alt="First slide"
+                        />
+                        <Carousel.Caption className="d-none d-md-block">
+                            <div classNameName="border-dark  border border-danger .d-sm-none .d-md-block" id='herocap'>
 
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="/img/newHero.png"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption className="d-none d-md-block">
-                        <div classNameName="border-dark  border border-danger .d-sm-none .d-md-block" id='herocap'>
-                            
-                            <h2 className='text-danger'>WordPress Project :</h2>
-                            <p>
-                                Project in use for client to raise money for first responders.
+                                <h2 className='text-danger'>WordPress Project :</h2>
+                                <p>
+                                    Project in use for client to raise money for first responders.
                             </p>
-                            <h1>
-                                <a href="https://heroaid.us/"><i className="bi bi-link-45deg"></i></a>
-                            </h1>
+                                <h1>
+                                    <a href="https://heroaid.us/"><i className="bi bi-link-45deg"></i></a>
+                                </h1>
+                            </div>
+
+                        </Carousel.Caption>
+                        <div className='smallCap text-center'>
+                            <a href="https://heroaid.us/" id='a'>WordPress.  <i className="bi bi-link-45deg"></i></a>
                         </div>
 
-                    </Carousel.Caption>
-                    <div className='smallCap text-center'>
-                    <a href="https://heroaid.us/" id ='a'>WordPress.  <i className="bi bi-link-45deg"></i></a>
-                    </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="/img/hooks.png"
+                            alt="Third slide"
+                        />
+                        <Carousel.Caption>
+                            <div className="border-dark  d-none d-md-block" id='hookscap' >
 
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="/img/hooks.png"
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <div className="border-dark  d-none d-md-block" id='hookscap' >
-                            
-                            <h2 className='text-light'>React Project With Hooks:</h2>
-                            <p id="pic" className="innerIam">
-                                A Mock E-commerce site made with React Hooks and APIs.
+                                <h2 className='text-light'>React Project With Hooks:</h2>
+                                <p id="pic" className="innerIam">
+                                    A Mock E-commerce site made with React Hooks and APIs.
                             </p>
-                            <h1>
-                                <a href="https://github.com/GrantHarris1/hooks"><i className="bi bi-github" ></i></a>
-                            </h1>
+                                <h1>
+                                    <a href="https://github.com/GrantHarris1/hooks"><i className="bi bi-github" ></i></a>
+                                </h1>
 
+                            </div>
+
+                        </Carousel.Caption>
+                        <div className='smallCap text-center'>
+                            <a href="https://github.com/GrantHarris1/hooks" id='a'>React w/Hooks.  <i className='bi bi-github'></i></a>
                         </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="/img/wftw.png"
+                            alt="Third slide"
+                        />
 
-                    </Carousel.Caption>
-                    <div className='smallCap text-center'>
-                    <a href="https://github.com/GrantHarris1/hooks" id ='a'>React w/Hooks.  <i className='bi bi-github'></i></a>
-                    </div>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="/img/wftw.png"
-                        alt="Third slide"
-                    />
+                        <Carousel.Caption>
+                            <div className="border-dark  d-none d-md-block" id='watercap' >
 
-                    <Carousel.Caption>
-                        <div className="border-dark  d-none d-md-block" id='watercap' >
-                            
-                            <h2 className='text-light'>WordPress Project :</h2>
-                            <p id="pic" className="innerIam">
-                                Project in use for a client that brings water to Africa and around the world. 
+                                <h2 className='text-light'>WordPress Project :</h2>
+                                <p id="pic" className="innerIam">
+                                    Project in use for a client that brings water to Africa and around the world.
                             </p>
-                            <h1>
-                                <a href="http://waterfortheworld-nla.org/" className='d-none d-lg-block'><i className="bi bi-link-45deg "></i></a>
-                            </h1>
-                        </div>
+                                <h1>
+                                    <a href="http://waterfortheworld-nla.org/" className='d-none d-lg-block'><i className="bi bi-link-45deg "></i></a>
+                                </h1>
+                            </div>
 
-                    </Carousel.Caption>
-                    <div className='smallCap text-center'>
-                    <a href="http://waterfortheworld-nla.org/" id ='a'>WordPress.<i className="bi bi-link-45deg"></i></a>
-                    </div>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="/img/fullmenu.png"
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <div className="Iam border-dark d-none d-md-block" id='menucap' >
-                           
-                            <h2 className='text-light'>React Project:</h2>
-                            <p id="pic" className="innerIam">
-                                A React Project,
-                                made with Bootstrap,
-                                using an API call
-                                rendering a menu based on time of day.<br />
-                            </p>
-                            <h1>
-                                <a href="https://github.com/GrantHarris1/menu"><i className="bi bi-github" ></i></a>
-                            </h1>
+                        </Carousel.Caption>
+                        <div className='smallCap text-center'>
+                            <a href="http://waterfortheworld-nla.org/" id='a'>WordPress.<i className="bi bi-link-45deg"></i></a>
                         </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="/img/fullmenu.png"
+                            alt="Third slide"
+                        />
+                        <Carousel.Caption>
+                            <div className="Iam border-dark d-none d-md-block" id='menucap' >
 
-                    </Carousel.Caption>
-                    <div className='smallCap text-center'>
-                    <a href="https://github.com/GrantHarris1/menu" id ='a'>React w/API , Cond. Rendering.  <i className='bi bi-github'></i></a>
-                    </div>
-                </Carousel.Item>
-                <Carousel.Item variant="dark">
-                    <img
-                        className="d-block w-100"
-                        src="/img/full KHP.png"
-                        alt="Third slide"
-                    />
-                    
-                    <Carousel.Caption>
-                        <div className="Iam border-dark d-none d-md-block" id='khpcap' >
-                            
-                            <h2 className='text-light'>WordPress Project :</h2>
-                            <p id="pic" className="innerIam">
-                                Project in use for customer to book AirBnB properties.
-                            </p>
-                            <h1>
-                                <a href="https://kentuckyhistoricproperties.com/"><i className="bi bi-link-45deg"></i></a>
-                            </h1>
+                                <h2 className='text-light'>React Project:</h2>
+                                <p id="pic" className="innerIam">
+                                    A React Project, made with Bootstrap, using an API call rendering a menu based on time of day.
+                                </p>
+                                <h1>
+                                    <a href="https://github.com/GrantHarris1/menu"><i className="bi bi-github" ></i></a>
+                                </h1>
+                            </div>
+
+                        </Carousel.Caption>
+                        <div className='smallCap text-center'>
+                            <a href="https://github.com/GrantHarris1/menu" id='a'>React w/API , Cond. Rendering.  <i className='bi bi-github'></i></a>
                         </div>
-                    </Carousel.Caption>
-                    <div className='smallCap text-center'>
-                    <a href="https://kentuckyhistoricproperties.com/" id ='a'>WordPress.  <i className='bi bi-link-45deg'></i></a>
-                    </div>
-                </Carousel.Item>
-            </Carousel>
-                <div id = 'probot'>
-                <h2 id = 'probot'>These Projects.</h2>
-                <p>
-                    Were all made by Grant Harris. There are Wordpress projects included to show working, functional
+                    </Carousel.Item>
+                    <Carousel.Item variant="dark">
+                        <img
+                            className="d-block w-100"
+                            src="/img/full KHP.png"
+                            alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                            <div className="Iam border-dark d-none d-md-block" id='khpcap' >
+
+                                <h2 className='text-light'>WordPress Project :</h2>
+                                <p id="pic" className="innerIam">
+                                    Project in use for customer to book AirBnB properties.
+                            </p>
+                                <h1>
+                                    <a href="https://kentuckyhistoricproperties.com/"><i className="bi bi-link-45deg"></i></a>
+                                </h1>
+                            </div>
+                        </Carousel.Caption>
+                        <div className='smallCap text-center'>
+                            <a href="https://kentuckyhistoricproperties.com/" id='a'>WordPress.  <i className='bi bi-link-45deg'></i></a>
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
+                <div id='probot'>
+                    <h2 id='probot'>These Projects.</h2>
+                    <p>
+                        Were all made by Grant Harris. There are Wordpress projects included to show working, functional
                     websites in use by commercial clients.<br />
                     The rest of the Projects were made in HTML, CSS, React, React Bootstrap, Bootstrap, and API calls.
 
                 </p>
                 </div>
-           
 
 
-        </Container>
-        </body>
+
+            </Container>
+
     )
 }
