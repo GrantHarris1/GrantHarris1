8f9ded8641578e953332edca6a76a00f858ede63
grantharriselectric <grantharriselectric@gmail.com>
2021-11-12 19:53:50 -0500

    linkedin

--- src/components/Headliner.js
+++ src/components/Headliner.js
@@ -15,7 +15,7 @@ export default function Headliner() {
                         <Nav className="me-auto">
                             <Nav.Link as={Link} to='/aboutme'>AboutMe</Nav.Link>
                             <Nav.Link as={Link} to='/projects'>Projects</Nav.Link>
-                            <Nav.Link href='https://github.com/GrantHarris1'><i class="bi bi-github text-secondary" id="git"></i></Nav.Link >
+                            <Nav.Link href='https://github.com/1GrantHarris1'><i class="bi bi-github text-secondary" id="git"></i></Nav.Link >
                             <Nav.Link href='https://www.linkedin.com/in/1grantharris1/'><i class="bi bi-linkedin text-secondary" id="git"></i></Nav.Link >
                             <Nav.Link
                                 data-tf-popup="HWfJz59o"
